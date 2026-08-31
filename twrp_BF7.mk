83f9ff7c2ef2a76cae5d1e09c5ef0673f2b23a11
pandagamingthethird-wq <pandagamingthethird@gmail.com>
2026-08-31 13:41:12 +0800

    Update and rename twrp_BF7.mk to vendorsetup.sh

--- twrp_BF7.mk
+++
@@ -1,2 +0,0 @@
-# twrp_BF7.mk
-# TWRP (TeamWin Recovery Project) configuration for the BF7 device.
---
+++ vendorsetup.sh
@@ -0,0 +1,60 @@
+export TARGET_ARCH="arm64"
+
+# Some about us
+export OF_MAINTAINER=Charles078728
+
+# Build environment stuffs
+export FOX_BUILD_DEVICE="OnePlus8T"
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+
+# Use magisk boot for patching
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+
+# We have a/b partitions
+export FOX_AB_DEVICE=1
+export FOX_VIRTUAL_AB_DEVICE=1
+export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
+
+# Screen specifications
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_CLOCK_POS=0
+export OF_SCREEN_H=2400
+export OF_STATUS_H=120
+
+# Device stuff
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_FBE_METADATA_MOUNT_IGNORE=1
+export OF_USE_LEGACY_BATTERY_SERVICES=1
+
+# Run a process after formatting data to work-around MTP issues
+export OF_RUN_POST_FORMAT_PROCESS=1
+
+# OTA for custom ROMs
+export OF_SUPPORT_ALL_PAYLOAD_OTA_UPDATES=1
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+
+# Quick Backup list
+export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
+
+# Set default Keymaster Version
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
+
+# Dont install AROMAFM
+export FOX_DELETE_AROMAFM=1
+
+# Add some extras
+export FOX_USE_ZIP_BINARY=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_BUSYBOX_BINARY=1
+export FOX_USE_BASH_SHELL=1
+export OF_USE_LZMA_COMPRESSION=1
+export FOX_USE_NANO_EDITOR=1
+export OF_DONT_KEEP_LOG_HISTORY=1
+export OF_NO_SPLASH_CHANGE=1
+export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
