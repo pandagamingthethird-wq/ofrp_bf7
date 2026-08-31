0aecf3e4c43df1456286db4e4b439c8b4f72f4bd
pandagamingthethird-wq <pandagamingthethird@gmail.com>
2026-08-31 13:39:20 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,2 +1,38 @@
-# device.mk
-# Device-specific makefile for the BF7 device.
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/tecno/BF7
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1-service.trustonic \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service \
+    bootctrl.mt6761 \
+    bootctrl.mt6761.recovery \
+    vendor.trustonic.tee@1.1-service \
+    vendor.mediatek.hardware.keymaster_attestation@1.1-service
+
+PRODUCT_PACKAGES += \
+    bootctrl
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
